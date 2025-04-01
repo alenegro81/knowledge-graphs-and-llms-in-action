af28c189a797605b63c28a9b7d355ede61d415d3
fxiii <fxiii@gitlab.com>
2025-04-01 18:16:59 +0200

    refex

--- chapters/ch12/listings/12.19 ReFeX implementation.py
+++ chapters/ch12/listings/12.19 ReFeX implementation.py
@@ -1,6 +1,6 @@
 import networkx as nx
 import numpy as np
-from collections import defaultdict
+import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
 
@@ -10,16 +10,21 @@ class ReFeX:
         self.correlation_threshold = correlation_threshold
 
     def extract_features(self, G):
-        features = self._extract_local_features(G)
+        features, features_names = self._extract_local_features(G)
 
-        egonet_features = self._extract_egonet_features(G)
+        egonet_features, egonet_features_names = self._extract_egonet_features(G)
+
         features = np.column_stack((features, egonet_features))
+        features_names = np.hstack((features_names, egonet_features_names))
 
         for iteration in range(self.max_iterations):
-            new_features = self._generate_recursive_features(G, features)
+            new_features, new_features_names = self._generate_recursive_features(G, features, features_names)
+
             features = np.column_stack((features, new_features))
-            features = self._prune_features(features)
-        return features
+            features_names = np.hstack((features_names, new_features_names))
+
+            features, features_names = self._prune_features(features, features_names)
+        return features, features_names
 
     def _extract_local_features(self, G):
         """Extract local (node-level) features"""
@@ -37,7 +42,7 @@ class ReFeX:
             else:
                 features[idx, 1] = features[idx, 2] = G.degree(node)
 
-        return features
+        return features, np.array(["degree", "in_degree", "out_degree"])
 
     def _extract_egonet_features(self, G):
         n_nodes = G.number_of_nodes()
@@ -48,9 +53,9 @@ class ReFeX:
             features[idx, 1] = ego.number_of_edges()  # Number of edges in egonet
             features[idx, 2] = nx.density(ego)  # Density of egonet
 
-        return features
+        return features, np.array(["ego_nodes", "ego_edges", "ego_density"])
 
-    def _generate_recursive_features(self, G, current_features):
+    def _generate_recursive_features(self, G, current_features, current_features_names):
         n_nodes = G.number_of_nodes()
         n_features = current_features.shape[1]
         new_features = np.zeros((n_nodes, n_features * 2))
@@ -63,9 +68,12 @@ class ReFeX:
             neighbor_feats = current_features[[list(G.nodes()).index(n) for n in neighbors]]
             new_features[idx, :n_features] = np.sum(neighbor_feats, axis=0)
             new_features[idx, n_features:] = np.mean(neighbor_feats, axis=0)
-        return new_features
 
-    def _prune_features(self, features):
+        new_features_names = np.hstack([np.char.add(current_features_names, ".sum"),
+                                        np.char.add(current_features_names, ".mean")])
+        return new_features, new_features_names
+
+    def _prune_features(self, features, features_names):
         scaler = StandardScaler()
         scaled_features = scaler.fit_transform(features)
 
@@ -78,4 +86,45 @@ class ReFeX:
                     to_remove.add(j)
 
         keep_features = list(set(range(features.shape[1])) - to_remove)
-        return features[:, keep_features]
+        return features[:, keep_features], features_names[keep_features]
+
+
+def create_fraud_network():
+    G = nx.Graph()
+
+    fraudsters = ['D', 'E', 'F', 'I']
+
+    # Add all nodes first
+    nodes = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+             'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T']
+
+    G.add_nodes_from(nodes)
+
+    for node in G.nodes():
+        G.nodes[node]['is_fraudster'] = node in fraudsters
+
+    edges = [
+        ('A', 'B'), ('A', 'G'), ('A', 'H'), ('A', 'I'), ('A', 'O'),
+        ('A', 'T'), ('B', 'D'), ('B', 'C'), ('D', 'E'), ('D', 'F'),
+        ('D', 'G'), ('E', 'F'), ('F', 'G'), ('G', 'I'), ('H', 'K'),
+        ('I', 'K'), ('I', 'N'), ('K', 'J'), ('L', 'M'), ('L', 'N'),
+        ('N', 'M'), ('O', 'P'), ('O', 'Q'), ('Q', 'R'), ('Q', 'S')
+    ]
+
+    G.add_edges_from(edges)
+
+    return G
+
+
+def main():
+    G = create_fraud_network()
+    refex = ReFeX()
+    data, columns = refex.extract_features(G)
+    # create a DataFrame using nodes and features names as rows and coumns labels
+    features = pd.DataFrame(data, columns=columns, index=G.nodes)
+    pd.set_option('display.max_columns', None)
+    print(features)
+
+
+if __name__ == '__main__':
+    main()
